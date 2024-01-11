19c7e7b92784dd589dbe42dec85b06777eafd72c
balbachl <lisajbalbach@gmail.com>
2024-01-11 13:20:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, CIT Students!");
+            Console.WriteLine("Have a great semester!");
+
+        }
+    }
+}
